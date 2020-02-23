3d481c25f8bd9590ff67e53fdc5cbefe6ee94ee2
Wad Werton <wad_werton@mail.ru>
2020-02-23 17:15:48 +0300

    feat: add solution task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
+    let units = 'zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split(' ');
+    let tens = 'twenty thirty forty fifty sixty seventy eighty ninety'.split(' ');
+    if (number < 20) return units[number];
+    let digit = number % 10;
+    if (number < 100) return tens[Math.floor(number / 10) - 2] + (digit ? ' ' + units[digit]: '');
+    if (number < 1000) return units[Math.floor(number / 100)] +' hundred' + (number % 100 === 0 ? '' : ' ' + toReadable(number % 100));
+    return toReadable(Math.floor(number / 1000) + ' thousand' + (number % 1000 !== 0 ? ' ' + toReadable(number % 1000) : ''));
+
 }
